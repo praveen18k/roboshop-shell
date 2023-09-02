f4c6cdc801c7a598b850432199988722f0cc64f4
Praveen Kothapally <praveenkothapally18@gmail.com>
2023-09-02 21:20:16 +0530

    added pk-practice folder

---
+++ pk-practice/mongodb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#user should have root access if not display warning message in terminal 
+#while installing store the logs
+##implement colors for user experience
+#before installing it is always good to check whether package is already installed or not
+#if installed skip, otherwise proceed for installation
+#check successfully installed or not
+
+
