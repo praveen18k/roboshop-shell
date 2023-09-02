9bca46e437e210bb6b0c83c5adc8027137eda3fe
Praveen Kothapally <praveenkothapally18@gmail.com>
2023-09-02 21:44:12 +0530

    added pk-practice folder

--- pk-practice/mongodb.sh
+++ pk-practice/mongodb.sh
@@ -7,4 +7,36 @@
 #if installed skip, otherwise proceed for installation
 #check successfully installed or not
 
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGSDIR=/tmp
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0"
+Y="\e[33"
+
+
+if [ $USERID -ne 0 ];
+then
+    echo -e "$Y ERROR: Install with root access $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+yum install git -y &>> $LOGFILE
+
+VALIDATE $? "Installation of Git"
+
+    
