c2e69e611db8f0d5669d56dcebaaba09bc2786f8
Praveen Kothapally <praveenkothapally18@gmail.com>
2023-09-19 01:42:52 +0530

    adding mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+
 DATE=$(date +%F)
 LOGSDIR=/tmp
-# /home/centos/shellscript-logs/script-name-date.log
+#home/centos/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$0
 LOGFILE=$LOGSDIR/$0-$DATE.log
 USERID=$(id -u)
@@ -11,43 +12,51 @@ G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
-if [ $USERID -ne 0 ];
+if [ $USERID -ne 0 ]
 then
-    echo -e "$R ERROR:: Please run this script with root access $N"
+    echo -e "$R ERROR: Please Run this script with root access $N"
     exit 1
 fi
 
+#Function name is VALIDATE & this validate the previous command and inform user it is success or failure
 VALIDATE(){
-    if [ $1 -ne 0 ];
+    #$1 --> it will receive the argument 1
+    if [ $1 -ne 0 ]
     then
-        echo -e "$2 ... $R FAILURE $N"
+        echo -e "$2...$R FAILURE $N"
         exit 1
-    else
-        echo -e "$2 ... $G SUCCESS $N"
-    fi
+    else 
+        echo -e "$2...$G SUCCESS $N"
+    fi  
 }
 
-
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
 
 VALIDATE $? "Copied MongoDB repo into yum.repos.d"
 
-yum install mongodb-org -y &>> $LOGFILE
+yum install mongodb-org -y &>>$LOGFILE
 
 VALIDATE $? "Installation of MongoDB"
 
-systemctl enable mongod &>> $LOGFILE
+systemctl enable mongod &>>$LOGFILE
 
 VALIDATE $? "Enabling MongoDB"
 
-systemctl start mongod &>> $LOGFILE
+systemctl start mongod &>>$LOGFILE
 
 VALIDATE $? "Starting MongoDB"
 
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOGFILE
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOGFILE
 
 VALIDATE $? "Edited MongoDB conf"
 
-systemctl restart mongod &>> $LOGFILE
+systemctl restart mongod &>>$LOGFILE
 
-VALIDATE $? "Restarting MonogoDB"
+VALIDATE $? "Restarting MongoDB"
+
+
+
+
+
+
+
