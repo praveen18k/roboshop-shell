248af9122a6c1080f48473a43ea94f5a20ccec76
Praveen <praveenkothapally18@gmail.com>
2024-04-08 11:24:49 +0530

    Updated mongodb file

--- mongodb.sh
+++ mongodb.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+
+DATE=$(date +%F)
+LOGDIR=/tmp
+SCRIPT_NAME=$0
+LOGFILE=$LOGDIR/$SCRIPT_NAME-$DATE.log
+Userid=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $Userid -ne 0 ]
+then
+    echo -e "$R Error:: run this with root access $N"
+    exit 1
+fi
+
+Validation() {
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2...$R failure $N"
+        exit 1
+    else
+        echo -e "$2...$G success $N"
+    fi
+}
