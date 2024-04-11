f25571f894d1376d3ba8c85748e20909d9082c6d
Praveen <praveenkothapally18@gmail.com>
2024-04-11 21:27:55 +0530

    adding new script

--- create-ec2.sh
+++ create-ec2.sh
@@ -12,4 +12,29 @@ DOMAIN_NAME=awsdevops.site
  do
     if [[ $i == "mongodb" || $i == "mysql" ]]
     then
-        INSTANCE_TYPE="t3.micro"
+        INSTANCE_TYPE="t3.medium"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+    echo "creating $i instance"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo "created $i instance: $IP_ADDRESS")
+
+    aws route53 change-resource-record-sets --hosted-zone-id Z098285010FMU6PDV8O9P --change-batch '
+    {
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                            "Name": "'$i.$DOMAIN_NAME'",
+                            "Type": "A",
+                            "TTL": 300,
+                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+                        }}]
+    }
+    '
+done
+
+# imporvement
+# check instance is already created or not
+# update route53 record
+    
